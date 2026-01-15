bf5a57ed350d5cd858c1e81d853783a5a14cc711
Allan Ratemo <allanisratemo@gmail.com>
2026-01-15 12:30:33 +0300

    init

--- lib/scraper.py
+++ lib/scraper.py
@@ -1,6 +1,9 @@
-from turtle import ht
 from bs4 import BeautifulSoup
 import requests
 
 headers = {'user-agent': 'my-app/0.0.1'}
 html = requests.get("https://flatironschool.com/", headers=headers)
+
+doc = BeautifulSoup(html.text, 'html.parser')
+
+print(doc.select('.heading-financier'))
